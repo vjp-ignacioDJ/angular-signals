b87ce6a160c55fcbd755faf6372735734931b969
nachodj23 <nachomonter68@gmail.com>
2023-07-19 19:40:13 +0200

    Last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'products',
-    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
-  },
-  {
     path: 'signals',
     loadChildren:()=> import('./signals/signals.module').then(m=> m.SignalsModule)
   },
   {
     path: '**',
-    redirectTo: 'products',
+    redirectTo: 'signals',
   },
 
 ];
